9cf083d38051e53a899a3a0940f6ad9203576e91
Josh Bebbington <josh@focalwebsites.com>
2015-02-03 15:06:50 +0000

    Dock has a capacity when initialised

---
+++ js/dock.js
@@ -0,0 +1,5 @@
+var Dock = function() {
+  this.capacity = 10;
+};
+
+module.exports = Dock;
--- spec/dockSpec.js
+++ spec/dockSpec.js
@@ -1,8 +1,15 @@
+var Dock = require('../js/dock')
+
 describe('Dock', function() {
 
   it('should have a capacity of 10', function() {
     var dock = new Dock();
     expect(dock.capacity).toEqual(10);
+  });
+
+  it('can dock a bike', function() {
+    dock.acceptBike(bike);
+    expect(dock.bikeCount).toEqual(1);
   });
 
 });
