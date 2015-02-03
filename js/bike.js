0f974e0bc6262dc700590369041037a6b04a25ca
matteomanzo <imatteo.manzo@gmail.com>
2015-02-03 15:02:26 +0000

    dock should have capacity of ten, test failing

--- js/bike.js
+++ js/bike.js
@@ -10,4 +10,8 @@ Bike.prototype.break = function() {
   this.broken = true;
 };
 
+Bike.prototype.fix = function() {
+  this.broken = false;
+};
+
 module.exports = Bike;
---
+++ spec/dockSpec.js
@@ -0,0 +1,8 @@
+describe('Dock', function() {
+
+  it('should have a capacity of 10', function() {
+    var dock = new Dock();
+    expect(dock.capacity).toEqual(10);
+  });
+
+});
